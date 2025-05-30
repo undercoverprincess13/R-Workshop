ffa947c7b3ab3b0140af8dce60f9b5e495ea20e2
laislemke <lalemke@stud.hs-heilbronn.de>
2025-05-30 14:33:46 +0200

    dash

--- shiny_dashboard/shiny_dashboard.R
+++ shiny_dashboard/shiny_dashboard.R
@@ -3,30 +3,44 @@ library(bslib)
 
 ui <- page_sidebar(
   title = "R Workshop - Coffee @ HHN",
-  sidebar = sidebar(
+  sidebar = sidebar("Filter",
+    selectInput(
+      "select widget",
+      "Geschlecht",
+      choices = list("Alle" = 1, "Frauen" = 2, "Männer" = 3),
+      selected = 1
+    ),
     sliderInput(
-      "slider",
+      "slider widget",
       "Alter der Befragten",
-      min = 0,
-      max = 100,
-      value = c(18, 40)
+      min = 18,
+      max = 35,
+      value = c(22, 28)
     )
   ),
   
   
   # Cards für Visualisierungen
   card(
-    card_header("Durchschnittliche Anzahl an Kaffee")
+    card_header("Durchschnittliche Anzahl an Kaffee"),
+    # Code
+    
+    
   ),
   card(
-    card_header("Korrelation Kaffeekonsum während und außerhalb der Prüfungsphase")
+    card_header("Korrelation Kaffeekonsum während und außerhalb der Prüfungsphase"),
+    # Code
+    
+    
   ),
   card(
-    card_header("Trinken Frauen mehr Kaffee als Männer bezogen auf Lernzeit (Hypothesentest / T-Test)")
+    card_header("Trinken Frauen mehr Kaffee als Männer bezogen auf Lernzeit (Hypothesentest / T-Test)"),
+    # Code
+    
+    
   )
 )
 
 server <- function(input, output) {}
 
 shinyApp(ui, server)
-
