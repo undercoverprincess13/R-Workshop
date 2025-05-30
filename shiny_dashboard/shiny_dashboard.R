4f7bdbf9e25b2805bdf5388675d41223a5e4b408
laislemke <lalemke@stud.hs-heilbronn.de>
2025-05-30 13:22:50 +0200

    dashboard

--- shiny_dashboard/shiny_dashboard.R
+++ shiny_dashboard/shiny_dashboard.R
@@ -1,51 +1,32 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
 library(shiny)
+library(bslib)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+ui <- page_sidebar(
+  title = "R Workshop - Coffee @ HHN",
+  sidebar = sidebar(
+    sliderInput(
+      "slider",
+      "Alter der Befragten",
+      min = 0,
+      max = 100,
+      value = c(18, 40)
     )
+  ),
+  
+  
+  # Cards für Visualisierungen
+  card(
+    card_header("Durchschnittliche Anzahl an Kaffee")
+  ),
+  card(
+    card_header("Korrelation Kaffeekonsum während und außerhalb der Prüfungsphase")
+  ),
+  card(
+    card_header("Trinken Frauen mehr Kaffee als Männer bezogen auf Lernzeit (Hypothesentest / T-Test)")
+  )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output) {}
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+shinyApp(ui, server)
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
-}
-
-# Run the application 
-shinyApp(ui = ui, server = server)
