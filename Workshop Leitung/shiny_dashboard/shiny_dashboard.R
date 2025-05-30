6d5cdf15ec0f442df02c15ea7396d2de75ce1f39
undercoverprincess13 <ellen.kistner@online.de>
2025-05-30 20:09:45 +0200

    T Test, Median hinzugefügt

--- Workshop Leitung/shiny_dashboard/shiny_dashboard.R
+++ Workshop Leitung/shiny_dashboard/shiny_dashboard.R
@@ -53,8 +53,43 @@ server <- function(input, output) {
   })
   
   # -------------------------
-  # Korrelationsmatrix
+  # Mittelwert und Median
   # ------------------------- 
+  output$stat_summary <- renderPrint({
+    df <- gefilterte_daten()
+    kaffee <- df$Kaffeetassen_pro_Tag
+    
+    summary <- list(
+      "Mittelwert" = mean(kaffee, na.rm = TRUE),
+      "Median" = median(kaffee, na.rm = TRUE),
+      "Standardabweichung" = sd(kaffee, na.rm = TRUE)
+    )
+    print(summary)
+  })
+  
+  # -------------------------
+  # Mittelwert und Median Boxplot
+  # ------------------------- 
+  output$boxplot_mittelwerte <- renderPlot({
+    df <- gefilterte_daten()
+    ggplot(df, aes(x = "", y = Kaffeetassen_pro_Tag)) +
+      geom_boxplot(fill = "#FFDDC1") +
+      stat_summary(fun = mean, geom = "point", shape = 20, size = 4, color = "red") +
+      labs(
+        title = "Boxplot mit Mittelwert",
+        y = "Tassen Kaffee pro Tag",
+        x = ""
+      ) +
+      theme_minimal()
+  })
+  
+  
+  
+  
+  # ------------------------------------------------------------------------------------------
+  # Korrelationsmatrix
+  # Wie hängen die Variablen Alter, Lernzeit, Kaffetassen, Geschlecht, Prüfungsphase zusammen 
+  # ------------------------------------------------------------------------------------------ 
   
   output$cor_matrix <- renderPlot({
     df <- gefilterte_daten()
@@ -97,6 +132,18 @@ server <- function(input, output) {
       ) +
       theme_minimal()
   })
+  
+  # --------------------------------------------------------------------------
+  # Hypothesentest
+  # Trinken Männer wirklich mehr Kaffee als Frauen?
+  # --------------------------------------------------------------------------
+  output$t_test_ergebnis <- renderPrint({
+    df <- gefilterte_daten()
+    t_test <- t.test(Kaffeetassen_pro_Tag ~ Geschlecht, data = df)
+    t_test
+    
+    
+  })
 
 }
 
@@ -127,36 +174,53 @@ ui <- page_sidebar(
 # Cards für Visualisierungen
 div(
   style = "height: 100vh; overflow-y: auto; padding: 1rem; box-sizing: border-box;",
-  card(
-    card_header("Deskiptive Statistik"),
-    p("Dieses Histogramm zeigt, wie viele Tassen Kaffee pro Tag konsumiert werden."),
-    plotOutput("hist_kaffee")
+  layout_columns(
+    col_widths = c(12),
+    card(
+      card_header("Deskiptive Statistik"),
+      p("Dieses Histogramm zeigt, wie viele Tassen Kaffee pro Tag konsumiert werden."),
+      plotOutput("hist_kaffee")
     
-    
+  )),
+  layout_columns(
+    col_widths = c(6,6),  
+    card(
+      card_header("Statistische Kennwerte"),
+      p("Mittelwert, Median und Standardabweichung des Kaffeekonsums in der gewählten Stichprobe."),
+      verbatimTextOutput("stat_summary")
+    ),
+    card(
+      card_header("Boxplot: Median + Mittelwert"),
+      p("Der Boxplot zeigt Median, Streuung (Quartile) und Ausreißer. Der rote Punkt ist der Mittelwert."),
+      plotOutput("boxplot_mittelwerte", height = "300px")
+    )
   ),
-layout_columns(
-  col_widths = c(6, 6),
-  
-  card(
-    card_header("Korrelation Kaffeekonsum während und außerhalb der Prüfungsphase"),
-    p("Diese Korrelationsmatrix zeigt Zusammenhänge zwischen Alter, Kaffee- und Lernverhalten."),
-    plotOutput("cor_matrix", height = "400px")
+  layout_columns(
+    col_widths = c(6, 6),
+    card(
+      card_header("Korrelation Kaffeekonsum während und außerhalb der Prüfungsphase"),
+      p("Diese Korrelationsmatrix zeigt Zusammenhänge zwischen Alter, Kaffee- und Lernverhalten."),
+      plotOutput("cor_matrix", height = "400px")
   ),
-  
-  card(
-    card_header("Regression"),
-    p("Zusammenhang zwischen Lernzeit und Kaffeekonsum"),
-    plotOutput("regression_plot", height = "400px")
-  )
-),
-  card(
-    card_header("Trinken Frauen mehr Kaffee als Männer bezogen auf Lernzeit (Hypothesentest / T-Test)"),
-    # Code
     
-    
+    card(
+      card_header("Regression"),
+      p("Zusammenhang zwischen Lernzeit und Kaffeekonsum"),
+      plotOutput("regression_plot", height = "400px")
+    )
+  ),
+  layout_columns(
+    col_widths = c(12),
+
+    card(
+      card_header("Trinken Frauen mehr Kaffee als Männer bezogen auf Lernzeit (Hypothesentest / T-Test)"),
+      p("Der T-Test prüft, ob es einen signifikanten Unterschied im Kaffeekonsum zwischen Männern und Frauen gibt."),
+      verbatimTextOutput("t_test_ergebnis")
+      )
+    )
   )
 )
-)
+
 
 # ----------------
 # APP STARTEN
